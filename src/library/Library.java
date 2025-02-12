b07fc8daeda4f749f30e66210a057dbd1af2745e
apaten0403 <alexispaten@gmail.com>
2025-02-11 23:06:24 -0600

    Added removeBook and addBook methods for Library

--- src/library/Library.java
+++ src/library/Library.java
@@ -1,5 +1,55 @@
 package library;
 
 public class Library {
-
+	private final int totalBooks = 5;
+	private Book[] books = new Book[totalBooks];
+	private int numOfBooks = 0;
+	
+	
+	/**
+	 *  This method will add a book to the Library object
+	 *  @param book
+	 */
+	public boolean addBook(Book book) {
+		if (numOfBooks == totalBooks || numOfBooks < 0) {
+			System.out.println("The Library is full! You can't add anymore books.");
+			return false;
+		}	
+		
+		if(books[numOfBooks] == null) {
+			books[numOfBooks] = book;
+			numOfBooks++;
+			return true;
+		}
+		
+		for(int i = 0; i < numOfBooks; i++) {
+			if(books[i] == null) {
+				books[i] = book;
+				numOfBooks++;
+				return true;
+			}
+		}
+		
+		return true;
+	}
+	
+	/** 
+	 * This method will remove a book from the Library object
+	 * @param book
+	 */
+	public boolean removeBook(Book book) {
+		if(numOfBooks <= 0) {
+			System.out.println("There are no books in the library right now. You can't remove any books!");
+			return false;
+		}
+		for(int i = 0; i < numOfBooks; i++) {
+			if(this.books[i].equals(book)) {
+				books[i] = null;
+				numOfBooks--;
+				return true;
+			}
+		}
+		return false;
+	}
+	
 }
